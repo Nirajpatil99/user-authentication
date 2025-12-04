cb7cc7c2721e9fc0e84987830c6407929f31b1fe
niraj.patil <niraj.patil@irisbusiness.com>
2025-12-04 12:25:43 +0530

    create the entity user

---
+++ src/main/java/com/example/user_authentication/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.user_authentication.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private boolean isactive;
+    private LocalDate createddate;
+
+}
